781fe814f39f34f1be0381b66d285ce6760be838
alhidalgog <alonso.hidalgo004@gmail.com>
2025-07-01 23:30:20 -0400

    se agrega registrar usuario y login

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 import os
 from bson.objectid import ObjectId
 
-load_dotenv()
+load_dotenv() #se carga el archivo .env, donde se ubican las credenciales de MongoDB
 
 app = Flask(__name__)
 
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client["foroFlask"]
-temas = db["temas"]
+client = MongoClient(os.getenv("MONGO_URI")) #se accede a MongoDB
+db = client["foroFlask"] #se accede a la BD foroFlask dentro de las credenciales accedidas anteriormente por client
+temas = db["temas"] #se accede a la colección "temas"
+usuarios = db["users"] #se accede a la colección "usuarios"
 count = temas.count_documents({})
 
 @app.route("/")
@@ -23,8 +24,8 @@ def index():
 def nuevo_tema():
     if request.method == "POST":
         titulo = request.form["titulo"]
+        creador = request.form["usuario"]
         contenido = request.form["contenido"]
-        creador = request.form["creador"]
         temas.insert_one({"titulo": titulo, "creador": creador, "contenido": contenido, "respuestas": []})
         return redirect(url_for("index"))
     return render_template("nuevo_tema.html")
@@ -42,6 +43,27 @@ def ver_tema(id):
         return redirect(url_for("ver_tema", id=id))
 
     return render_template("tema.html", tema=tema)    
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if(request.method == "POST"):
+        correo = request.form["correo"]
+        password = request.form["password"]
+        if(correo == usuarios.find_one({correo}) and password == usuarios.find_one({password})):
+            return redirect(url_for("index"))
+    return render_template("login.html")
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if(request.method == "POST"):
+        nombre = request.form["nombre"]
+        correo = request.form["correo"]
+        password = request.form["password"]
+        usuarios.insert_one({"nombre": nombre, "correo": correo, "password": password})
+        return redirect(url_for("login"))
+    return render_template("registro.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
